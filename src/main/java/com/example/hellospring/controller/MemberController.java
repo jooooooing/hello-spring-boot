3970ed050fc55de63db36f98f6a19b84dee7cb5d
jooooooing <leegun1018@naver.com>
2022-08-31 17:49:41 +0900

    도메인, 리포지토리 생성(테스트)

---
+++ src/main/java/com/example/hellospring/controller/MemberController.java
@@ -0,0 +1,8 @@
+package com.example.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    
+}
---
+++ src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,55 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //DI
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+
+    private void validateDuplicateMember(Member member) {
+        //같은 이름이 있는 중복 회원 x
+        //null의 가능성이 있으면 Optional로 감싸서 반환
+        //아래와 같은 의미의 코드. findByName을 했을때 값이 있다면 이미 존재하는 ~ 반환
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> {
+//        throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+}
