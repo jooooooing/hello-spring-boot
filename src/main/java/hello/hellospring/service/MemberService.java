6928c7fc47eb809c24c0083fb7aafece5a6dbe74
jooooooing <leegun1018@naver.com>
2022-10-08 23:23:23 +0900

    Feat : AOP 적용
    시간 측정(ms) AOP 적용

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,59 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+//@Service
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //DI
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+
+    private void validateDuplicateMember(Member member) {
+        //같은 이름이 있는 중복 회원 x
+        //null의 가능성이 있으면 Optional로 감싸서 반환
+        //아래와 같은 의미의 코드. findByName을 했을때 값이 있다면 이미 존재하는 ~ 반환
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> {
+//        throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+}
